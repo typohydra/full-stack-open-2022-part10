e781dad242c582c30151710e5310598634447e69
typohydra <74554736+typohydra@users.noreply.github.com>
2022-12-13 13:30:59 +0400

    exercise 10.16 sign out

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,8 +1,12 @@
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Pressable } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 import theme from "../theme";
 import { Link } from "react-router-native";
+
+import useSignOut from "../hooks/useSignOut";
+import { useQuery } from "@apollo/client";
+import { ME } from "../graphql/queries";
 
 const styles = StyleSheet.create({
   container: {
@@ -18,15 +22,26 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const [signOut] = useSignOut();
+  const { data } = useQuery(ME);
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal styles={styles.scrollView}>
         <Link to="/">
           <AppBarTab>Repositories</AppBarTab>
         </Link>
-        <Link to="/signin">
-          <AppBarTab>Sign in</AppBarTab>
-        </Link>
+        {data?.me ? (
+          <Link to="/signin">
+            <Pressable onPress={() => signOut()}>
+              <AppBarTab>Sign out</AppBarTab>
+            </Pressable>
+          </Link>
+        ) : (
+          <Link to="/signin">
+            <AppBarTab>Sign in</AppBarTab>
+          </Link>
+        )}
       </ScrollView>
     </View>
   );
---
+++ rate-repository-app/src/hooks/useSignOut.js
@@ -0,0 +1,17 @@
+import { useApolloClient } from "@apollo/client";
+
+import useAuthStorage from "../hooks/useAuthStorage";
+
+const useSignOut = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    apolloClient.resetStore();
+  };
+
+  return [signOut];
+};
+
+export default useSignOut;
